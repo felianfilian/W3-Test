f7ab695936d17c7fe2d2f1825b040fea9517daf7
Dennis Schmalstieg <darianbellentor@outlook.de>
2025-05-03 01:55:12 +0200

    new project

--- nodetest.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Welcome to dungeon");
-    res.end("hello world");
-  })
-  .listen(8080);
